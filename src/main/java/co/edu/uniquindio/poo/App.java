6c7764664779fcb0ef3bb1c12414ec9469adc870
ValeryOrtiz <valery.comortiz@gmail.com>
2024-03-05 12:06:48 -0500

    Creacion de nueva instancia

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -6,6 +6,8 @@ package co.edu.uniquindio.poo;
  */
 public class App {
     public static void main(String[] args) {
+        Pets pet1 = new Pets("Abril",Species.CAT,Breed.ANGORA,(byte)4,Gender.FEMALE,Colour.BLACK,4.2);
         System.out.println("Hello World!");
+        System.out.println(pet1);
     }
 }
